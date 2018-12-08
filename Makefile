1f625f70fbbef4b92d9e4e4a0422178084f89933
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-12-08 16:33:28 +0200

    Added motion control example program from BNO055 serial input

--- Makefile
+++ Makefile
@@ -6,6 +6,12 @@ compile-landscape:
 	g++ -std=c++11 -o simu ./landscape_gen.cpp ./worldmath.cpp ./graphics.cpp ./data_io.cpp -lm -lSDL2
 pgcompile-landscape:
 	g++ -std=c++11 -o simu ./landscape_gen.cpp ./worldmath.cpp ./graphics.cpp ./data_io.cpp -lm -lSDL2 -pg -ggdb
+compile-sensor:
+	g++ -std=c++11 -o sens ./sensor_control.cpp ./worldmath.cpp ./graphics.cpp ./data_io.cpp -lm -lSDL2 -lboost_system
+pgcompile-sensor:
+	g++ -std=c++11 -o sens ./sensor_control.cpp ./worldmath.cpp ./graphics.cpp ./data_io.cpp -lm -lSDL2 -lboost_system -pg -ggdb
+run-sensor:
+	./sens
 run-landscape:
 	./simu
 run:
@@ -14,5 +20,7 @@ crun: compile
 	make run
 crun-landscape: compile-landscape
 	make run-landscape
+crun-sensor: compile-sensor
+	make run-sensor
 profile: pgcompile
 	make run
---
+++ sensor_control.cpp
@@ -0,0 +1,115 @@
+#include <boost/asio/serial_port.hpp>
+#include <boost/asio.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "blocking_reader.h"
+#include "graphics.h"
+#include "worldmath.h"
+#include "data_io.h"
+
+using namespace std;
+using namespace boost;
+
+Point3D U = Point3D(0, 1, 0, 0); // Camera True Up vector
+Point3D R = Point3D(1, 0, 0, 0); // Camera True Right vector
+Point3D D = Point3D(0, 0, 1, 0); // Camera True Directional vector
+float dx = 0.0;					 // Player x position
+float dy = 0.0;					 // Player y position
+float dz = 0.0;					 // Player z position
+
+float qx = 0.0;
+float qy = 0.0;
+float qz = 0.0;
+
+int main()
+{
+	asio::io_service io;
+	asio::serial_port port(io);
+
+	port.open("/dev/ttyACM0");
+	port.set_option(asio::serial_port_base::baud_rate(9600));
+
+	char c;
+	int calibration = 0;
+	bool done = false;
+	graphicsStartup();
+	LoadObject((char *)"sensor_shape.txt");
+	Matrix *P = new Matrix(1, 15, M_PI * 5.0 / 12.0, 3.0 / 4.0);
+	time_t start, end;
+	float zBuffer[WIDTH * HEIGHT];
+	while (!done)
+	{
+		start = clock();
+		blocking_reader reader(port, 500);
+
+		string rsp;
+
+		while (reader.read_char(c) && c != '\n')
+		{
+			rsp += c;
+		}
+
+		if (c != '\n')
+		{
+			cout << "Read Timed out!" << endl;
+		}
+		float tqx, tqy, tqz;
+		if (rsp[0] != 'O')
+		{
+			sscanf(rsp.c_str(), "%f%f%f%d", &tqx, &tqy, &tqz, &calibration);
+		}
+		// cout << tqx << endl;
+
+		if (calibration > 0)
+		{
+			qx = tqx * (M_PI / 180);
+			qy = tqy * (M_PI / 180);
+			qz = tqz * (M_PI / 180);
+		}
+
+		for (int i = 0; i < HEIGHT; i++)
+		{
+			for (int j = 0; j < WIDTH; j++)
+			{
+				int index = i * WIDTH + j;
+				zBuffer[index] = 1.0;
+			}
+		}
+		graphicsFrameReady();
+		Matrix V = viewMatrix(U, R, D, dx, dy, dz);
+		objects[0].drawObject(qz, qy, qx, 0, 0, -3, P, &V, zBuffer);
+		graphicsFrameDraw();
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				done = true;
+				break;
+			case SDL_KEYUP:
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					done = true;
+					break;
+				}
+			}
+		}
+		end = clock();
+		double diff = difftime(end, start);
+		if (diff < 16667)
+		{
+			int wait = 16667 - diff;
+			usleep(wait);
+		}
+	}
+	port.close();
+	graphicsShutdown();
+	return 0;
+}
